553032a725fd503a90cc713d31f2c667681ed1e8
argellius <adamcerny0@gmail.com>
2021-01-07 21:34:49 +0100

    Zprovoznění tlačítek v menu

--- app/src/main/java/com/example/mojepocasi_utb/MainActivity.kt
+++ app/src/main/java/com/example/mojepocasi_utb/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.mojepocasi_utb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnVyhledat = findViewById(R.id.button_pocasi) as Button
+        btnVyhledat.setOnClickListener {
+            val intent = Intent(this, Activity_VypisPocasi::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+        val btnHistorie = findViewById(R.id.button_Historie) as Button
+        btnHistorie.setOnClickListener {
+            //Historie
+        }
+
+
+
+        val btnKonec = findViewById(R.id.button_konec) as Button
+        btnKonec.setOnClickListener {
+            super.finish()
+        }
     }
 }
