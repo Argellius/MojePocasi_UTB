920c4d034a5d67ba9279a4066289fd24329040e2
argellius <adamcerny0@gmail.com>
2021-01-07 21:17:39 +0100

    Initial commit Založení activit - splashscree, vypis pocasi a main activity

---
+++ app/src/main/java/com/example/mojepocasi_utb/Activity_SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.mojepocasi_utb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__splash_screen)
+    }
+}
---
+++ app/src/main/java/com/example/mojepocasi_utb/Activity_VypisPocasi.kt
@@ -0,0 +1,11 @@
+package com.example.mojepocasi_utb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_VypisPocasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__vypis_pocasi)
+    }
+}
