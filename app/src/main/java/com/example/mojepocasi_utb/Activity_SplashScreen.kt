6c935007f303c7681d234f57795364f1e73e66b5
argellius <adamcerny0@gmail.com>
2021-01-07 21:32:24 +0100

    Zprovoznění splashscreenu

--- app/src/main/java/com/example/mojepocasi_utb/Activity_SplashScreen.kt
+++ app/src/main/java/com/example/mojepocasi_utb/Activity_SplashScreen.kt
@@ -1,11 +1,20 @@
 package com.example.mojepocasi_utb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Activity_SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__splash_screen)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2500)
+
     }
 }
